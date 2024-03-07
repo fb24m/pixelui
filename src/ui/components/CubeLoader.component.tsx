6adf3e449946030a1d7d85bec558c082ba1bca47
iFB24M <fakem1ner@yandex.ru>
2024-03-07 23:23:09 +0400

    add CubeLoader

--- src/ui/components/CubeLoader.component.tsx
+++ src/ui/components/CubeLoader.component.tsx
@@ -10,10 +10,10 @@ export const CubeLoader = ({ className, children, speed = 3, ...props }: Skeleto
 	return (
 		<>
 			<div className={`relative block w-[60px] h-[60px]`}>
-				<div className={`${cubeClass} [animation-name:cube\\_1] [animation-duration:1s] [animation-iteration-count:infinite] top-0 left-0`}></div>
-				<div className={`${cubeClass} [animation-name:cube\\_2] [animation-duration:1s] [animation-iteration-count:infinite] top-0 right-0`}></div>
-				<div className={`${cubeClass} [animation-name:cube\\_3] [animation-duration:1s] [animation-iteration-count:infinite] bottom-0 left-0`}></div>
-				<div className={`${cubeClass} [animation-name:cube\\_4] [animation-duration:1s] [animation-iteration-count:infinite] bottom-0 right-0`}></div>
+				<div className={`${cubeClass} [animation-name:cube1] [animation-duration:1s] [animation-iteration-count:infinite] top-0 left-0`}></div>
+				<div className={`${cubeClass} [animation-name:cube2] [animation-duration:1s] [animation-iteration-count:infinite] top-0 right-0`}></div>
+				<div className={`${cubeClass} [animation-name:cube3] [animation-duration:1s] [animation-iteration-count:infinite] bottom-0 left-0`}></div>
+				<div className={`${cubeClass} [animation-name:cube4] [animation-duration:1s] [animation-iteration-count:infinite] bottom-0 right-0`}></div>
 			</div>
 		</>
 	)
